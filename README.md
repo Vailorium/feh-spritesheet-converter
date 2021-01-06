7b815906776361de181972228e3af4a95d96bb38
Vailorium <casterthe5587@gmail.com>
2021-01-06 19:43:50 +1300

    final

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # feh-spritesheet-converter
- Transforms FEH spritesheets into seperated images
+Transforms FEH spritesheets into separated images
+
+## Requirements
+`webptools` must be installed: https://pypi.org/project/webptools/
+
+## Usage
+.plist and .png files should be put into the input folder
+Example input folder (using Item and Item_2 from assets/Common/UI:
+```
+input/
+	* Item.plist
+	* Item.png
+	* Item_2.plist
+	* Item_2.png
+main.py
+```
+
+Separated images are exported into the output folder
+
+Run using `py/python main.py`
---
+++ main.py
@@ -0,0 +1,47 @@
+import os, webptools, copy, json, plistlib, time
+from PIL import Image
+
+
+# despite the fact that the images say they're png, they're webp files and need to be converted
+def convertImages():
+
+    
+    for filename in os.listdir('./input'):
+        if '.png' in filename:
+            webptools.dwebp('./input/'+filename, './input/'+filename, '-o')
+
+def transformImages():
+    if not os.path.isdir('./output'):
+        os.mkdir('./output')
+    
+    for filename in os.listdir('./input'):
+        if ".plist" in filename:
+            with open("./input/"+filename, 'rb') as listfile:
+                pl = plistlib.load(listfile)
+                img = Image.open("./input/"+filename.split('.')[0]+".png")
+                count = 0
+                for key, value in pl['frames'].items():
+                    count += 1
+                    if count != 1:
+                        print ("\033[A                             \033[A")
+                    print("{} ({}/{}) - {}".format(filename, count, len(pl['frames']), key))
+                    time.sleep(0.05)
+                    im = copy.copy(img)
+                    textureRect = json.loads(value['textureRect'].replace('{', '[').replace('}', ']'))
+
+                    left = textureRect[0][0]
+                    top = textureRect[0][1]
+                    right = textureRect[0][0] + textureRect[1][0]
+                    bottom = textureRect[0][1] + textureRect[1][1]
+
+                    seperated = im.crop((left, top, right, bottom))
+
+                    if value['textureRotated']:
+                        seperated = seperated.rotate(90)
+
+                    seperated.save('./output/'+key, "PNG")
+def main():
+    convertImages()
+    transformImages()
+
+main()
